acd0ec9c4cc84113a5828d547d5d7580641c4d0a
Umar Mustafa <umustafa@c2r8s4.42lisboa.com>
2026-09-14 13:21:14 +0100

    changed ex0

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -17,6 +17,7 @@ def main() -> None:
     print(f"Program name: {sys.argv[0]}")
     if len(sys.argv) == 1:
         print("No arguments provided!")
+        print("Total arguments: 1")
         print()
         return
     else:
